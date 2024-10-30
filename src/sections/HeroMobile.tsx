a46439aa55a2904631419b2e079b83ea59580a8b
Chirath abeyamanna <chamodyachirath@gmail.com>
2024-10-30 20:34:08 +0530

    added readme file

--- src/sections/HeroMobile.tsx
+++ src/sections/HeroMobile.tsx
@@ -41,23 +41,25 @@ function HeroMobile() {
   }, []);
   return (
     <div className="relative h-max top-[3rem] bg-background  pb-20 overflow-x-hidden">
-      <div className="relative flex justify-center items-center pb-[9vh] Profile  w-[100vw] h-[100vh]  text-white font-poppins">
+      <div className="relative flex justify-center mt-[20%] pb-[15vh] Profile  w-[100vw] h-max  text-white font-poppins">
         <div>
-          <div className="flex justify-center h-max">
+          <div className="relative justify-center h-max ">
             <h1
               id="nameMobile"
-              className="relative text-[11vw]  top-20 font-guriella tracking-wider text-name font-bold"
+              className="relative text-[11vw] font-guriella tracking-wider text-name font-bold"
             >
               Chamodya Chirath
             </h1>
           </div>
-          <div className="relative top-[2vw] flex justify-center h-max m-10 ">
-            <button className="relative p-2 font-spartan font-medium text-white text-[4vw] ">
-              <span className="relative flex h-[2vw] w-[2vw] -left-4 top-[3vw]">
+          <div className="relative flex justify-center h-max mb-5">
+            <button className="relative font-spartan font-medium text-white text-[4vw]">
+              <span className="absolute top-[25%] flex h-[2vw] w-[2vw]">
                 <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
                 <span className="relative inline-flex rounded-circle h-[2vw] w-[2vw] bg-green-400"></span>
               </span>
-              <span className="relative top-0 ml-3 ">Availabe for Hire</span>
+              <span className="relative top-0 ml-3 ">
+                Availabe for a part time position
+              </span>
             </button>
           </div>
           <div className="relative top-[1vh]  flex justify-center h-max ">
@@ -71,19 +73,19 @@ function HeroMobile() {
           </div>
           <div className="flex justify-center h-max ">
             <h2 className="text-[5vw] font-guriella text-[#bde2fa]">
-              Software Engineer / Web Developer
+              Software Engineer | Web Developer
             </h2>
           </div>
         </div>
       </div>
-      <div className="about me h-[100vh] w-[100vw] pr-8">
+      <div className=" h-max w-[100vw] pr-8 ">
         <TextAnimation
           text=""
           paragraph="I am a Computer Science undergraduate with a passion for creating innovative solutions. Currently, I am specializing in Robotics Engineering and Machine Learning, while also nurturing a strong interest in web development, which I am pursuing independently."
           changeSpeed={0.02}
         />
       </div>
-      <div className="relative h-max bg-background rounded-md  mr-5 ml-5">
+      <div className="relative h-max bg-background rounded-md  mr-5 ml-5 -mt-20">
         <div className="relative top-0 left-0 w-full h-max ">
           <div
             className={`project-card h-max p-[8vw] rounded-md bg-[#101014] text-white overflow-hidden mb-[3vh]`}
@@ -178,9 +180,13 @@ function HeroMobile() {
                 </h2>
               </div>
               <h2 className="pt-[3vh] mb-2 text-xl flex h-max justify-end ">
-                <a href="CV.pdf" download="Chamodya_Chirath.pdf">
+                <a
+                  href="CV.pdf"
+                  download="Chamodya_Chirath.pdf"
+                  className=" relative pl-3"
+                >
                   <button className="flex h-max  text-[4vw] hover:text-[#4c77ab]">
-                    <div className="w-[2vw] h-[2vw] bg-[#4c77ab] mr-2 rounded-circle  transition-[ease-in-out] "></div>{" "}
+                    <div className="absolute top-[30%] left-0 w-[2vw] h-[2vw] bg-[#4c77ab] mr-2 rounded-circle  transition-[ease-in-out] "></div>{" "}
                     Get CV
                   </button>
                 </a>
--- src/sections/hero.tsx
+++ src/sections/hero.tsx
@@ -19,7 +19,7 @@ const Hero = () => {
       <head>
         <link rel="preload" href="CV.pdf" as="document" />
       </head>
-      <div className="header flex relative">
+      <div className="header flex relative h-max">
         <div
           className={`font-Kameron text-white ml-5 mt-5 text-[clamp(40px,3vw,900px)] -space-y-1 tracking-wide ${
             animate ? "fade-in-up" : ""
@@ -44,7 +44,7 @@ const Hero = () => {
               <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
               <span className="relative inline-flex rounded-full h-3 w-3 bg-green-400"></span>
             </span>
-            <div className="ml-3 ">Availabe for Hire</div>
+            <div className="ml-3 ">Availabe for Part time position</div>
           </button>
         </div>
       </div>
@@ -57,19 +57,19 @@ const Hero = () => {
       ></div>
 
       {/* About card */}
-      <div className="absolute top-[35vh] left-5 flex justify-start  w-[67vw] h-max ">
+      <div className="relative mt-16 left-5 flex justify-start  w-[67vw] h-max">
         <div
-          className={`about-card relative w-[40vw] p-5 mr-6 mb-10 pt-[clamp(2px,1vh,50px)] rounded-md bg-card text-white overflow-hidden`}
+          className={`about-card relative w-[40vw] p-5 mr-6 pt-[clamp(2px,1vh,50px)] rounded-md bg-card text-white overflow-hidden`}
         >
           <div
             className={`${
               animate ? "fade-in-up fade-delay-1" : ""
             } overflow-hidden`}
           >
-            <h2 className="text-[clamp(15px,1.5vw,100px)] text-xl mb-5">
+            <h2 className="text-[clamp(25px,1.5vw,100px)] mb-5">
               Little About me
             </h2>
-            <p className="relative text-[clamp(10px,1vw,70px)] space-y-8 mb-2 mt-5 leading-5 text-[#CFCFCF] overflow-hidden">
+            <p className="relative text-[clamp(15px,1vw,70px)] space-y-8 mb-2 mt-5 leading-5 text-[#CFCFCF] overflow-hidden">
               I am a Computer Science undergraduate with a passion for creating
               innovative solutions. Currently, I am specializing in Robotics
               Engineering and Machine Learning, while also nurturing a strong
@@ -78,33 +78,33 @@ const Hero = () => {
               <br /> This portfolio showcases my skills, interests, and
               dedication to growth in these fields
             </p>
-            <p className="text-[clamp(12px,1.5vw,20px)] text-center w-max text-[#CFCFCF]  overflow-hidden">
+            <p className="text-[clamp(18px,1.5vw,20px)] text-center w-max text-[#CFCFCF]  overflow-hidden">
               - To be great you have to give it you’re all and a little bit{" "}
               <span className="text-white font-semibold">more</span>. -
             </p>
           </div>
         </div>
         <div
-          className={`absolute right-5 top-10 ink-card w-max h-max  p-[2vw] rounded-md bg-[#101014] text-white overflow-hidden`}
+          className={`relative mr-2 top-10 ink-card w-max h-max  p-[2vw] rounded-md bg-[#101014] text-white overflow-hidden`}
         >
           <div
             className={`relative ${animate ? "fade-in-up fade-delay-2" : ""}`}
           >
-            <div className="pl-10">
-              <h1 className="h-max  font-poppins text-cente r">
+            <div className="">
+              <h1 className="h-max text-[clamp(10px,1.3vw,100px)] font-poppins text-center">
                 Looking for a new Talent ?
               </h1>
             </div>
-            <div className="relative w-max h-max mb-[4vh] ">
-              <h2 className="text-2xl pt-3 transition-colors hover:text-[#8078d3]">
+            <div className="relative h-max mb-[4vh] ">
+              <h2 className="text-[clamp(15px,1.2vw,100px)] text-center pt-3 transition-colors hover:text-[#8078d3]">
                 <a href="mailto:chamodyachiarth@gmail.com">
                   {" "}
                   chamodyachirath@gmail.com{" "}
                 </a>
               </h2>
             </div>
-            <div className="absolute w-max h-max pb-10  top-20  right-0 ">
-              <h2 className=" pt-3 mb-9 text-xl ">
+            <div className="absolute w-max h-max  top-20  right-0 ">
+              <h2 className="pt-3  text-[clamp(15px,1.3vw,100px)] ">
                 <a href="CV.pdf" download="Chamodya_Chirath.pdf">
                   <button className="flex hover:text-[#4c77ab]">
                     <div className="w-[10px] h-[10px] bg-[#4c77ab] mr-2 rounded-circle mt-2 transition-[ease-in-out] "></div>{" "}
@@ -118,38 +118,38 @@ const Hero = () => {
       </div>
 
       {/* Project Cards */}
-      <div className="absolute top-[70vh] left-0 w-full h-max flex justify-around">
+      <div className="relative mt-10 mb-10 left-0 w-full h-max flex justify-around">
         <div
-          className={`project-card w-[12vw] h-[20vh] p-[2vw] rounded-md bg-[#101014] text-white overflow-hidden`}
+          className={`project-card w-[12vw] h-max p-[2vw] pb-2 rounded-md bg-[#101014] text-white overflow-hidden`}
         >
           <div
             className={`relative ${animate ? "fade-in-up fade-delay-2" : ""}`}
           >
-            <h1 className="relative h-max text-[3vw]  font-Lexend text-center">
+            <h1 className="relative h-max text-[3vw]  font-Lexend text-center ">
               <Counter endNumber={NoProject} />
             </h1>
-            <p className="relative -top-2 text-[12px] space-y-8 leading-5 text-[#CFCFCF] text-center">
+            <p className="relative -top-2 text-[15px]  text-[#CFCFCF] text-center ">
               projects completed
             </p>
           </div>
         </div>
 
         <div
-          className={`Achievement-card w-[12vw] h-[20vh] p-[2vw]  rounded-md bg-[#101014] text-white overflow-hidden`}
+          className={`Achievement-card w-max h-max p-[2vw]  rounded-md bg-[#101014] text-white overflow-hidden`}
         >
           <div
             className={`relative block ${
               animate ? "fade-in-up fade-delay-2" : ""
             }`}
           >
-            <div className="relative flex mb-8 h-12">
+            <div className="relative flex mb-2 h-12">
               <h1 className="font-Lexend text-[3vw] text-center flex">
                 <i className="relative text-clamp fa fa-trophy mr-1 top-2"></i>
                 <Counter endNumber={NoAchivements} />
               </h1>
             </div>
-            <div className="relative -top-1">
-              <p className=" text-[12px] space-y-8 -ml-2 text-[#CFCFCF] text-center">
+            <div className="relative top-5 pb-2">
+              <p className=" text-[15px]  text-[#CFCFCF] text-center">
                 Achievements
               </p>
             </div>
@@ -157,7 +157,7 @@ const Hero = () => {
         </div>
 
         <div
-          className={`Skills-card w-[12vw] h-[20vh] p-2 pt-8 rounded-md bg-[#101014] text-white overflow-hidden`}
+          className={`Skills-card w-max h-max p-8 pb-2 rounded-md bg-[#101014] text-white overflow-hidden`}
         >
           <div
             className={`relative ${animate ? "fade-in-up fade-delay-2" : ""}`}
@@ -165,13 +165,13 @@ const Hero = () => {
             <h1 className="text-[3vw] mb-5 font-Lexend text-center">
               <Counter endNumber={NoSkills} />
             </h1>
-            <p className="relative -top-2 text-[12px] space-y-8  leading-5 text-[#CFCFCF] text-center">
+            <p className="relative -top-2 text-[15px] space-y-8  leading-5 text-[#CFCFCF] text-center">
               Skills collected
             </p>
           </div>
         </div>
         <div
-          className={`Tech-card w-[12vw] h-[20vh] p-2 pt-8 rounded-md bg-[#101014] text-white`}
+          className={`Tech-card w-max h-max p-2 pt-8 rounded-md bg-[#101014] text-white`}
         >
           <div
             className={`relative ${animate ? "fade-in-up fade-delay-2" : ""}`}
@@ -179,7 +179,7 @@ const Hero = () => {
             <h1 className="text-[clamp(20px,3vw,45px)] mb-5 font-Lexend text-center">
               TECH
             </h1>
-            <p className="relative -top-2 text-[12px] space-y-8 leading-5 text-[#CFCFCF] text-center">
+            <p className="relative -top-2 text-[15px] space-y-8 leading-5 text-[#CFCFCF] text-center">
               Enthusiast
             </p>
           </div>
